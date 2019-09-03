72704db4ebdec43a84a1e5fd3e840e2bad5493d6
Mijail <mijail0824@gmail.com>
2019-09-03 17:28:57 -0500

    Se creo servicio rest con firebase yse cargan datos para mostrarlos en el about component

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,26 +8,54 @@ import { InfoPagina } from '../interfaces/info-pagina.interfaces';
 })
 export class InfoPaginaService {
 
+equipo:any [] = [];  
 info: InfoPagina = {};
 cargada = false;
 
   constructor(private _http : HttpClient) {
 
-    console.log("Servicio listo y cargado");
+    
 
     // leer el Json
+    this.cargaInfo();
+    this.cargarEquipo();
+
+
+  }
+
+  private cargaInfo() {
 
     this._http.get('assets/data/data-pagina.json')
               .subscribe( (data : InfoPagina) =>{
-                
-                this.cargada = true;
-                this.info = data;
-                console.log(data);
+      
+      this.cargada = true;
+      this.info = data;
+      //console.log(data);
 
-              })
+    });
 
-   }
+  }
 
+  private cargarEquipo(){
+    //https://angular-html-3d827.firebaseio.com/equipo.json
+    //https://angular-html-3d827.firebaseio.com/equipo.json
+    this._http.get('https://angular-html-3d827.firebaseio.com/equipo.json')
+              .subscribe( (resp : any[]) =>{
+      
+      this.cargada = true;      
+      this.equipo = resp;      
+      //console.log(this.equipo);
+    
+    },
+    error =>{
+      console.log(error);
+
+    }
+    );
+
+  }
+
+  
 
 
 
